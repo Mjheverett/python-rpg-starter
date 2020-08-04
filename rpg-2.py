61670a7a33d8381a8c3dfe5b9793020c6d02b1ca
Matthew Everett <matthew@mjheverett.com>
2020-08-04 14:53:15 -0400

    updates

--- rpg-2.py
+++ rpg-2.py
@@ -1,11 +1,12 @@
-"""
+print("""
 In this simple RPG game, the hero fights the goblin. He has the options to:
 
 1. fight goblin
 2. do nothing - in which case the goblin will attack him anyway
 3. flee
 
-"""
+""")
+
 class Warrior:
     def __init__(self, health, power):
         self.health = health
